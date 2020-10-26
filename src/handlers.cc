f00b1365960505d9be9349796692bcf6f14cfbf8
Daniel Wykerd <danielwykerd@gmail.com>
2020-10-26 16:41:36 +0200

    Ignore empty messages

--- src/handlers.cc
+++ src/handlers.cc
@@ -61,12 +61,8 @@ bz_free_random_auction_flips(random_flips);
 
 void message_create_handler(aegis::gateway::events::message_create obj) {
     try {
-        //get snowflakes related to this message
-        // C++17 version
-        //const auto [channel_id, guild_id, message_id, member_id] = obj.msg.get_related_ids();
-        const aegis::snowflake channel_id = obj.msg.get_channel().get_id();
-        const aegis::snowflake guild_id = obj.msg.get_guild().get_id();
-        const aegis::snowflake message_id = obj.msg.get_id();
+        if (obj.msg.get_content() == "") return; // ignore empty messages
+
         aegis::core & bot = obj.msg.get_guild().get_bot();
         const std::string mention = obj.msg.get_user().get_mention();
 #if !defined(AEGIS_DISABLE_ALL_CACHE)
@@ -82,8 +78,6 @@ void message_create_handler(aegis::gateway::events::message_create obj) {
         if (obj.msg.is_bot() || !obj.msg.has_guild()) return;
 
         auto & _channel = obj.msg.get_channel();
-        auto & _guild = _channel.get_guild();
-        auto & username = obj.msg.author.username;
 
         std::string content{obj.msg.get_content()};
         // Transform command to lowercase
